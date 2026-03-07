8a2a0cceb8f27213a1629fad49cf3602cd0cdd82
7aib <zohaibrana03@gmail.com>
2026-03-07 18:51:36 +0500

    fix: typo

--- app/robots.ts
+++ app/robots.ts
@@ -9,6 +9,6 @@ export default function robots(): MetadataRoute.Robots {
         disallow: ["/api/", "/_next/"],
       },
     ],
-    sitemap: "https://mastertechnicals.ae/sitemap.xml",
+    sitemap: "https://mastertechnicals.com/sitemap.xml",
   }
 }
